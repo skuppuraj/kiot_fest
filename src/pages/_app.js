5d49d0e44e1ba1b37884951a48a2176f18c129e6
Kuppuraj <kuppuraj@live.in>
2026-09-16 07:38:13 +0530

    feat(16): apply hydration-safe localStorage cart persistence and suppressHydrationWarning

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../styles/globals.css';
 import { Provider } from 'react-redux';
 import { store } from '../redux/store';
+import { hydrateCart } from '../redux/slices/cartSlice';
+import { loadState } from '../redux/localStorage';
 import CartDrawer from '../components/CartDrawer';
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    // Safely hydrate cart from localStorage on the client after initial mount
+    const savedState = loadState();
+    if (savedState && savedState.cart) {
+      store.dispatch(hydrateCart(savedState.cart));
+    }
+  }, []);
+
   return (
     <Provider store={store}>
       <Component {...pageProps} />
